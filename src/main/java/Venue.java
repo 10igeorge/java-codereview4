722555844e2ad190af6dbc2ef4805c6c4b4c7e92
10igeorge <10izzygeorge@gmail.com>
2016-03-03 15:31:37 -0800

    pass test saving venue into database and returning list of venues

--- src/main/java/Venue.java
+++ src/main/java/Venue.java
@@ -16,4 +16,33 @@ public class Venue {
   public String getVenueName(){
     return venue_name;
   }
+
+  @Override
+  public boolean equals(Object newVenue) {
+  if (newVenue instanceof Venue) {
+    Venue otherVenue = (Venue) newVenue;
+    return this.getVenueName().equals(otherVenue.getVenueName()) &&
+      this.getId() == otherVenue.getId();
+    } else {
+      return false;
+    }
+  }
+
+  public static List<Venue> all(){
+  String sql = "SELECT * FROM venues";
+  try(Connection con = DB.sql2o.open()){
+    return con.createQuery(sql)
+    .executeAndFetch(Venue.class);
+    }
+  }
+
+  public void save(){
+    String sql = "INSERT INTO venues (venue_name) VALUES (:venue_name)";
+    try(Connection con = DB.sql2o.open()){
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("venue_name", venue_name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
 }
---
+++ src/test/java/VenueTest.java
@@ -0,0 +1,59 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class VenueTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void save_savesVenueIntoDatabase(){
+    Venue venue = new Venue("Ash Street Saloon");
+    venue.save();
+    assertEquals(Venue.all().get(0), venue);
+  }
+
+  // @Test
+  // public void all_returnsAllVenues_emptyAtFirst() {
+  //   assertEquals(Venue.all().size(), 0);
+  // }
+  //
+  // @Test
+  // public void all_ensuresAllVenuesAreSavedCorrectly(){
+  //   Venue firstVenue = new Venue("Ash Street Saloon");
+  //   Venue secondVenue = new Venue("Doug Fir");
+  //   firstVenue.save();
+  //   secondVenue.save();
+  //   assertEquals(Venue.all().size(), 2);
+  // }
+  //
+  // @Test
+  // public void find_findsVenueInDatabase(){
+  //   Venue firstVenue = new Venue("Ash Street Saloon");
+  //   Venue secondVenue = new Venue("Doug Fir");
+  //   firstVenue.save();
+  //   secondVenue.save();
+  //   Venue savedVenue = Venue.find(firstVenue.getId());
+  //   assertTrue(savedVenue.equals(firstVenue));
+  // }
+  //
+  // @Test
+  // public void delete_deletesVenueFromDatabase(){
+  //   Venue firstVenue = new Venue("Ash Street Saloon");
+  //   Venue secondVenue = new Venue("Doug Fir");
+  //   firstVenue.save();
+  //   secondVenue.save();
+  //   firstVenue.delete();
+  //   assertFalse(Venue.all().contains(firstVenue));
+  // }
+  //
+  // @Test
+  // public void update_updatesVenueInfo(){
+  //   Venue firstVenue = new Venue("Ash Street Pub");
+  //   firstVenue.save();
+  //   firstVenue.update("Ash Street Saloon");
+  //   Venue savedVenue = Venue.find(firstVenue.getId());
+  //   assertEquals(savedVenue.getVenueName(), "Ash Street Saloon");
+  // }
+}
